d32e43dcf0f4bd8fae1f83df256d2c1e473de18a
William Mead <william.mead@manchenumerique.fr>
2024-02-14 11:32:13 +0100

    Added contributor details

--- class/actions_thirdpartydelivery.class.php
+++ class/actions_thirdpartydelivery.class.php
@@ -1,5 +1,6 @@
 <?php
 /* Copyright (C) 2023 SuperAdmin
+ * Copyright (C) 2024   William Mead    <william.mead@manchenumerique.fr>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
